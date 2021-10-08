6720c7b44dbc0a511d7e64cbcbea1233f134af58
joamartico <joamartico@gmail.com>
2021-10-08 02:38:06 -0300

    fixed routing bug when signing in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { IonApp, IonPage, IonRouterOutlet, IonSpinner, IonSplitPane, useIonRouter } from '@ionic/react';
+import {
+  IonApp,
+  IonPage,
+  IonRouterOutlet,
+  IonSpinner,
+  IonSplitPane,
+  useIonRouter,
+} from '@ionic/react';
 
 import { IonReactRouter } from '@ionic/react-router';
 import { useEffect, useState } from 'react';
@@ -7,9 +14,9 @@ import { Body } from '../constants/styledComponents';
 import { COLORS } from '../constants/theme';
 import { authentication } from '../firebase';
 import Context from './context/ContextComponent';
-import Onboarding from "./pages/Onboarding";
+import Onboarding from './pages/Onboarding';
 import SignIn from './pages/SignIn';
-import SignUp from "./pages/SignUp";
+import SignUp from './pages/SignUp';
 import Tabs from './pages/Tabs';
 
 const App = () => {
@@ -31,8 +38,20 @@ const App = () => {
 
   if (initialRoute === undefined) {
     return (
-      <div style={{background: COLORS.bg, height: "100vh", width: "100vw", position: "fixed", top: 0, left: 0, display: "flex", justifyContent: 'center', alignItems: 'center', }}>
-          <IonSpinner />
+      <div
+        style={{
+          background: COLORS.bg,
+          height: '100vh',
+          width: '100vw',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <IonSpinner />
       </div>
     );
 
@@ -52,17 +71,14 @@ const App = () => {
           <IonReactRouter>
             <IonSplitPane contentId="main">
               <IonRouterOutlet id="main">
-                {/* {initialRoute == 'Onboarding' && <Redirect to="/signin" />} */}
-                <Route
-                  path="/tabs"
-                  render={() => (initialRoute == 'Onboarding' ? <Onboarding /> : <Tabs />)}
-                />
+                <Route path="/tabs" render={() => <Tabs />} />
                 <Route path="/signin" render={() => <SignIn />} />
                 <Route path="/signup" render={() => <SignUp />} />
                 <Route path="/onboarding" render={() => <Onboarding />} />
-                <Route exact path="/" render={() => <Redirect to="/tabs/week" />} />
+                <Route exact path="/" render={() => initialRoute=="Onboarding"?<Redirect to="/onboarding"/>:<Redirect to="/tabs/week"/>} />
               </IonRouterOutlet>
             </IonSplitPane>
+            {initialRoute == 'Onboarding' && <Redirect to="/onboarding" />}
           </IonReactRouter>
         </IonApp>
       </Context>
