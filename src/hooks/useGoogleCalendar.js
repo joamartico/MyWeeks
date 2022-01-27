d1fbb7d8ace22e2a948b23d64e9922c3b5d37014
joamartico <joamartico@gmail.com>
2022-01-26 22:48:19 -0300

    complete token storaged and using it when create event

--- src/hooks/useGoogleCalendar.js
+++ src/hooks/useGoogleCalendar.js
@@ -45,8 +45,8 @@ const useGoogleCalendar = () => {
     await gapi.auth2.getAuthInstance().signIn();
     // const new_access_token = await gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token
     const new_access_token = await gapi.auth.getToken()
-    console.log('new_access_token', new_access_token.access_token);
-    setToken(new_access_token.access_token);
+    console.log('new_access_token', new_access_token);
+    setToken(new_access_token);
   }
 
   function signOut() {
@@ -66,7 +66,10 @@ const useGoogleCalendar = () => {
 
     if (true) {
       // await gapi.auth2.setToken(access_token);
-      await gapi.auth.setToken({access_token: token.access_token});
+      // await gapi.auth.setToken({access_token: token.access_token});
+
+      await gapi.auth.setToken(token);
+
       // await gapi.client.setToken(access_token);
       // await gapi.auth.setToken({access_token: token });
       // await gapi.client.setToken({access_token: token});
